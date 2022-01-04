4e0716809b6c19526968e0f007aa4a3cd6f5a2f2
Brian Garland <briangarlandk@gmail.com>
2022-01-04 01:01:01 -0600

    Get request to find all tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(productData);
   } catch(err) {
-    res.status(500).json(error)
+    res.status(500).json(err)
   }
   });
 
